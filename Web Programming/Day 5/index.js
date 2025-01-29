46492edfaa69e939aa6ea6fce0c8fd8e9223a4cd
Darla03 <157409266+Darla03@users.noreply.github.com>
2025-01-29 13:38:07 -0600

    Add files via upload

---
+++ Web Programming/Day 5/index.js
@@ -0,0 +1,9 @@
+console.log("Hello World");
+console.log("The answer is " +42);
+alert("Message!");
+
+
+let level =23; 
+let accuracy = 0.99;
+let name = "Pikachu";
+let temp = [55, 60, 57,5] //array
