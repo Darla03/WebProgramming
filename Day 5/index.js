d678ed2a94a62d90dce9a1b8f53c156128cfbfe1
Darla03 <157409266+Darla03@users.noreply.github.com>
2025-01-30 19:08:28 -0600

    jan30

--- Day 5/index.js
+++ Day 5/index.js
@@ -4,3 +4,21 @@ alert("Message!");
 
 
 let level =23; 
+
+
+
+
+
+
+
+
+
+let name=["Earth", "Water","Fire"];
+let array=[];
+name[0]="Hello ";
+name[2]="MY";
+
+array.push("Darlene");
+array.unshift("Tyler");
+array.pop(); //delete last element (from the end)
+array.sort();
