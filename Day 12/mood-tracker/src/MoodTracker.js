0fd8e3d89b59825f80d46fd08cf73e38f7d1a739
Darla03 <157409266+Darla03@users.noreply.github.com>
2025-03-03 20:30:58 -0600

    updates

---
+++ Day 12/mood-tracker/src/MoodTracker.js
@@ -0,0 +1,32 @@
+import {useState} from 'react';
+
+export default function MoodTracker(){
+    const [mood,setMood] =useState("😃");
+
+    return(
+        <div stylr={{textAlign:"center",padding: "20px"}}>
+            <h1>Mood Tracker</h1>
+            <p>Current Mood: {mood}</p>
+            <button 
+            onClick={()=>setMood("😭")} 
+            style={{margin:"5px", 
+            padding:"10px", background:"lightgreen"}} >very happy 
+            </button>
+           <button onClick={()=>setMood("🥺")} 
+            style={{margin:"5px", 
+            padding:"10px", background:"lightblue"}} > sad
+            </button>
+
+            <button onClick={()=>setMood("😒")} 
+            style={{margin:"5px", 
+            padding:"10px", background:"lightblue"}} > unamused
+            </button>
+
+            <button onClick={()=>setMood("🤓")} 
+            style={{margin:"5px", 
+            padding:"10px", background:"lightblue"}} > nerdy
+            </button>
+
+        </div>
+    );
+}
