bab93dae5e45f42119d57a27f5b0ffade003d7cd
Darla03 <157409266+Darla03@users.noreply.github.com>
2025-03-10 13:44:14 -0500

    code along Day 14

--- Day 14/my-express-app/server.js
+++ Day 14/my-express-app/server.js
@@ -2,21 +2,57 @@ const express = require('express');
 const app = express();
 
 const users = [
-    {id:1, name: "Alice"},
-    {id:2, name: "Bob"},
+    {id: 1, name: "Alice"},
+    {id: 2, name: "Bob"},
 ];
 
-const products= [
-    {id:1, name: "Laptop", price:999},
-    {id:2, name: "Phone", price: 699},
+const products = [
+    {id: 1, name: "Laptop", price: 999},
+    {id: 2, name: "Phone", price: 699},
 ];
 
-//Routing...
-app.get("/", (req,res) =>
-res.send("Welcome to my API!"));
+const shoes = [
+    {id: 1, name: "Nike", price: 100},
+    {id: 2, name: "addidas", price: 85},
+];
 
-//start server 
-const PORT =3000;
-app.listen(PORT,()=>
-console.log('Server running on http://localhost:${PORT}'));
+const fruits = [
+    {id: 1, name: "Apple", price: 2},
+    {id: 2, name: "Pear", price: 3},
+];
 
+const haircolor = [
+    {id: 1, name: "Black", price: 2},
+    {id: 2, name: "Brown", price: 3},
+];
+
+// Routing...
+app.get("/", (req, res) =>
+    res.send("Welcome to my API!")
+);
+
+app.get("/users", (req, res) =>
+    res.json(users)
+);
+
+app.get("/products", (req, res) =>
+    res.json(products)
+);
+
+app.get("/products/shoes", (req, res) =>
+    res.json(shoes)
+);
+
+app.get("/fruits", (req, res) =>
+    res.json(fruits)
+);
+
+app.get("/haircolor", (req, res) =>
+    res.json(haircolor)
+);
+
+// Start server 
+const PORT = 3000;
+app.listen(PORT, () =>
+    console.log(`Server running on http://localhost:${PORT}`)
+);
